8b760d76a77b261ee474a28ff7efcf495ffcbf02
Dewei Liu <deweiliu@hotmail.com>
2020-02-06 20:03:50 +0000

    change to lower case

--- git/git-commit.sh
+++ git/git-commit.sh
@@ -1,19 +1,20 @@
-message(){
+function message(){
     echo "COMMIT MESSAGE:"
-    echo "    $1"
+    eval msg=$1
+    echo "    ${msg}"
     echo
 }
 execute(){
     # print the command before executing
     cmd="$1"
-    echo "---> ${cmd}"
-    ${cmd}
+    echo ${cmd[@]}
+    ${cmd[@]}
 }
-message $1
+commit_message=$1
+message \${commit_message}
 execute "git config --global user.email 'deweiliu@hotmail.com'"
-execute "git config --global user.name 'Dewei Liu'"
+execute "git config --global user.name 'Dewei Liu' --replace-all"
 execute "git add ."
-execute 'git commit -m "$1"'
+execute "git commit -m '${commit_message}'"
 execute "git push origin HEAD:master"
-echo "git commit & push DONE"
-
+echo "git commit & pus
