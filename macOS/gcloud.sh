0678127edc131b81af4aeb8982c02c845223740d
Dewei Liu <deweiliu@hotmail.com>
2020-03-14 00:24:52 +0000

    add Macos configuration script

---
+++ macOS/gcloud.sh
@@ -0,0 +1,15 @@
+cd ~
+
+# Download GCloud SDK
+curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-284.0.0-darwin-x86_64.tar.gz
+
+tar -xv -f ./google-cloud-sdk-284.0.0-darwin-x86_64.tar.gz
+
+rm -f google-cloud-sdk-284.0.0-darwin-x86_64.tar.gz
+
+./google-cloud-sdk/install.sh --usage-reporting false --path-update true --command-completion true --bash-completion true --quiet
+
+gcloud --version
+
+gcloud components update --quiet
+
---
+++ macOS/mac-config.sh
@@ -0,0 +1,1 @@
+bash gcloud.sh
